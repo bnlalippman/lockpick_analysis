be456281a7f570ab9c39a8be342c84de7059d24b
bnlalippman <alippman@bnl.gov>
2025-12-05 08:06:51 -0500

    added comments

--- decrypt/decrypt.py
+++ decrypt/decrypt.py
@@ -4,29 +4,44 @@ PATH = "/home/alippman/Lockpick/forela-criticaldata"
 KEY = "bhUlIshutrea98liOp"
 
 def decrypt(data):
+    # An array of ints 0-255 to represent
     decrypted = bytearray(len(data))
+    # Get length of our decryption key
     key_length = len(KEY)
+    # Create our key as a byte string
     key_as_bytes = KEY.encode()
-    for i in range(len(data)):
-        decrypted[i] = data[i] ^ key_as_bytes[i % key_length]
+    
+    # For byte in data 
+    for byte in range(len(data)):
+        # Take each byte of file data and XOR by each key byte
+        # The modulo (%) is used to jump back to beginning of key
+        decrypted[byte] = data[byte] ^ key_as_bytes[byte % key_length]
     return bytes(decrypted)
 
 def main():
+    # Gather list of absolute paths for files under PATH
     files = os.listdir(PATH)
     file_absolute_paths = [os.path.join(PATH, file) for file in files]
+    
+    # Iterate over each file
     for file in file_absolute_paths:
+        # If encrypted
         if file.endswith('24bes'):
-            print(file)
+            print(f'[*] Decrypting {file}')
+            # Open and read encrypted file in binary read mode
             with open(file, "rb") as f:
                 encrypted_data = f.read()
             
+            # Decrypt data using our XOR function
             plaintext = decrypt(encrypted_data)
 
+            # Strip the encrypted file extension before writing out
             decrypted_file = file.strip('.24bes')
-            print(decrypted_file)
 
+            # Write out the decrypted binary data to new file
             with open(decrypted_file, "wb") as f:
                 f.write(plaintext)
+            print(f'[+] Decrypted {decrypted_file}')
 
 if __name__ == '__main__':
     main()
--- encrypt/encrypt.py
+++ encrypt/encrypt.py
@@ -1,32 +1,47 @@
 import os
 
-PATH = "/home/alippman/Lockpick/forela-criticaldata"
+PATH = "/path/to/files"
 KEY = "bhUlIshutrea98liOp"
 
 def encrypt(data):
+    # An array of ints 0-255 to represent
     encrypted = bytearray(len(data))
+    # Get length of our encryption key
     key_length = len(KEY)
+    # Create our key as a byte string
     key_as_bytes = KEY.encode()
-    for i in range(len(data)):
-        encrypted[i] = data[i] ^ key_as_bytes[i % key_length]
+    
+    # For byte in data 
+    for byte in range(len(data)):
+        # Take each byte of file data and XOR by each key byte
+        # The modulo (%) is used to jump back to beginning of key
+        encrypted[byte] = data[byte] ^ key_as_bytes[byte % key_length]
     return bytes(encrypted)
 
 def main():
+    # Gather list of absolute paths for files under PATH
     files = os.listdir(PATH)
     file_absolute_paths = [os.path.join(PATH, file) for file in files]
+    
+    # Iterate over each file
     for file in file_absolute_paths:
-        if not file.endswith('24bes'):
-            print(file)
+        # If file ends in txt
+        if file.endswith('txt'):
+            print(f'[*] Encrypting {file}')
+            # Open and read encrypted file in binary read mode
             with open(file, "rb") as f:
-                plaintext_data = f.read()
+                encrypted_data = f.read()
+            
+            # Encrypt data using our XOR function
+            plaintext = encrypt(encrypted_data)
 
-            encrypted_data = encrypt(plaintext_data)
+            # Strip the encrypted file extension before writing out
+            encrypted_file = file.strip('.24bes')
 
-            encrypted_file = file + '.24bes'
-            print(encrypted_file)
-
+            # Write out the encrypted binary data to new file
             with open(encrypted_file, "wb") as f:
-                f.write(encrypted_data)
+                f.write(plaintext)
+            print(f'[+] Encrypted {encrypted_file}')
 
 if __name__ == '__main__':
     main()
